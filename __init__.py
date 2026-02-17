dbdd9ad0a7f561a5109b9b60220a15e836a3e02a
Mohamad <sm.mirhoseininejad@gmail.com>
2026-02-16 20:40:38 -0500

    Add run_full_scan convenience function

--- __init__.py
+++ __init__.py
@@ -1,4 +1,14 @@
 from .validator import DatasetValidator
 from .scanner import DatasetScanner
 
-__all__ = ["DatasetValidator", "DatasetScanner"]
+def run_full_scan(dataset_root):
+    """
+    Convenience function to run both validation and scanning in one pipeline.
+    """
+    validator = DatasetValidator(dataset_root)
+    validator.validate_all()
+    
+    scanner = DatasetScanner(dataset_root)
+    return scanner.scan_dataset()
+
+__all__ = ["DatasetValidator", "DatasetScanner", "run_full_scan"]
